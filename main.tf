c8ccc876c0bd1120815acb7aba3a23b2542a5149
Uwe Grawert <grawert@b1-systems.de>
2020-09-17 11:30:56 +0200

    Use Octavia Loadbalancer

--- main.tf
+++ main.tf
@@ -2,7 +2,10 @@ terraform {
   required_version = ">= 0.12"
 }
 
-provider "openstack" {}
+provider "openstack" {
+  insecure = true
+  use_octavia = true
+}
 
 data "openstack_identity_auth_scope_v3" "scope" {
   name = "scope"
