7ba6e5b39f777955c49b8455da9fad54ce99d1a8
Uwe Grawert <grawert@b1-systems.de>
2020-09-27 14:20:04 +0200

    Add ProxyJump for master and worker nodes to ssh_config

--- ssh_config.tf
+++ ssh_config.tf
@@ -1,9 +1,18 @@
+locals {
+  hosts = {
+    for instance in concat(openstack_compute_instance_v2.master,
+                           openstack_compute_instance_v2.worker):
+      instance.name => instance.network.0.fixed_ip_v4
+  }
+}
+
 resource "local_file" "bastion_ssh_config" {
   filename = "ssh_config"
   file_permission = "0644"
   content = templatefile("${path.module}/template.d/ssh_config.tpl", {
-    ip_address = openstack_compute_floatingip_associate_v2.bastion.floating_ip
+    hosts = local.hosts
     ssh_login_user = var.ssh_login_user
     ssh_identity_file = var.ssh_identity_file
+    ip_address = openstack_compute_floatingip_associate_v2.bastion.floating_ip
   })
 }
--- template.d/ssh_config.tpl
+++ template.d/ssh_config.tpl
@@ -6,3 +6,14 @@ Host bastion
   ForwardAgent yes
   StrictHostKeyChecking no
   UserKnownHostsFile /dev/null
+%{for hostname, ip in hosts ~}
+Host ${hostname}
+  Hostname ${ip}
+  User ${ssh_login_user}
+  IdentityFile ${ssh_identity_file}
+  IdentitiesOnly yes
+  ForwardAgent yes
+  ProxyJump bastion
+  StrictHostKeyChecking no
+  UserKnownHostsFile /dev/null
+%{endfor}
